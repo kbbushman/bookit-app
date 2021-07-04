7a4a07c7a2d979e04457ba28bd939b7188e39f42
kbbushman <k.bushman@yahoo.com>
2021-07-03 22:59:48 -0700

    Adds test create rental dispatch button

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,7 +1,7 @@
 import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import RentalCard from '../components/rental/RentalCard';
-import { fetchRentals } from '../actions';
+import { fetchRentals, createRental } from '../actions';
 
 function HomePage() {
   const dispatch = useDispatch();
@@ -17,10 +17,29 @@ function HomePage() {
     ));
   };
 
+  const handleAddRental = () => {
+    const rental = {
+      _id: Math.random().toString(32).slice(2),
+      title: 'Downtown view',
+      city: 'San Francisco',
+      category: 'condo',
+      image: 'http://via.placeholder.com/350x250',
+      numOfRooms: 2,
+      shared: false,
+      description: 'Very nice apartment in center of the city.',
+      dailyPrice: 79,
+    };
+
+    dispatch(createRental(rental));
+  };
+
   return (
     <div className="card-list">
       <h1 className="page-title">Your Home All Around the World</h1>
       <div className="row">{renderRentals()}</div>
+      <button className="btn btn-success" onClick={handleAddRental}>
+        Add Rental
+      </button>
     </div>
   );
 }
