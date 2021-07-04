84f37c1d899c4415b88263476ef2ad6a5e08fefc
kbbushman <k.bushman@yahoo.com>
2021-07-03 22:37:50 -0700

    Refactor removes local state with temp data, adds redux store

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,44 +1,15 @@
-import { useState } from 'react';
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import RentalCard from '../components/rental/RentalCard';
-
-const initialState = [
-  {
-    _id: '1',
-    title: 'Nice view on ocean',
-    city: 'San Francisco',
-    category: 'condo',
-    image: 'http://via.placeholder.com/350x250',
-    numOfRooms: 4,
-    shared: true,
-    description: 'Very nice apartment in center of the city.',
-    dailyPrice: 43,
-  },
-  {
-    _id: '2',
-    title: 'Modern apartment in center',
-    city: 'New York',
-    category: 'apartment',
-    image: 'http://via.placeholder.com/350x250',
-    numOfRooms: 1,
-    shared: false,
-    description: 'Very nice apartment in center of the city.',
-    dailyPrice: 11,
-  },
-  {
-    _id: '3',
-    title: 'Old house in nature',
-    city: 'Bratislava',
-    category: 'house',
-    image: 'http://via.placeholder.com/350x250',
-    numOfRooms: 5,
-    shared: true,
-    description: 'Very nice apartment in center of the city.',
-    dailyPrice: 23,
-  },
-];
+import { fetchRentals } from '../actions';
 
 function HomePage() {
-  const [rentals] = useState(initialState);
+  const dispatch = useDispatch();
+  const rentals = useSelector((state) => state.rentals);
+
+  useEffect(() => {
+    dispatch(fetchRentals());
+  }, []);
 
   const renderRentals = () => {
     return rentals.map((rental) => (
