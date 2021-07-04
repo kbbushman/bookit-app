9c1599c1650aa3e5d913fefa8765934dd05ad6a2
kbbushman <k.bushman@yahoo.com>
2021-07-04 01:05:21 -0700

    Adds fetch one rental action and action creator

--- src/actions/rentals.js
+++ src/actions/rentals.js
@@ -1,12 +1,24 @@
 import { rentalData } from '../store/data';
 
 export const FETCH_RENTALS = 'FETCH_RENTALS';
+export const FETCH_ONE_RENTAL = 'FETCH_ONE_RENTAL';
 export const CREATE_RENTAL = 'CREATE_RENTAL';
 
 export function fetchRentals() {
   return {
     type: FETCH_RENTALS,
     rentals: rentalData,
+  };
+}
+
+export function fetchOneRental(id) {
+  const rental = rentalData.find((rental) => {
+    return rental._id === id;
+  });
+
+  return {
+    type: FETCH_ONE_RENTAL,
+    rental,
   };
 }
 
