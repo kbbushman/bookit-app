ef02ce42cf8015035f11a6dbc47c8a139fdbaef4
kbbushman <k.bushman@yahoo.com>
2021-07-03 17:10:56 -0700

    Adds HomePage component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Header from './components/shared/Header';
+import HomePage from './pages/HomePage';
 
 function App() {
   return (
     <div>
       <Header />
+      <HomePage />
     </div>
   );
 }
