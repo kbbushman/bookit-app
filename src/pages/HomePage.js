e86b3221a062023744673f98f1b5a9cc8c537fd1
kbbushman <k.bushman@yahoo.com>
2021-07-03 17:39:33 -0700

    Refactor adds temp rental data and RentalCard component

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,64 +1,56 @@
+import { useState } from 'react';
+import RentalCard from '../components/rental/RentalCard';
+
+const initialState = [
+  {
+    _id: '1',
+    title: 'Nice view on ocean',
+    city: 'San Francisco',
+    category: 'condo',
+    image: 'http://via.placeholder.com/350x250',
+    numOfRooms: 4,
+    shared: true,
+    description: 'Very nice apartment in center of the city.',
+    dailyPrice: 43,
+  },
+  {
+    _id: '2',
+    title: 'Modern apartment in center',
+    city: 'New York',
+    category: 'apartment',
+    image: 'http://via.placeholder.com/350x250',
+    numOfRooms: 1,
+    shared: false,
+    description: 'Very nice apartment in center of the city.',
+    dailyPrice: 11,
+  },
+  {
+    _id: '3',
+    title: 'Old house in nature',
+    city: 'Bratislava',
+    category: 'house',
+    image: 'http://via.placeholder.com/350x250',
+    numOfRooms: 5,
+    shared: true,
+    description: 'Very nice apartment in center of the city.',
+    dailyPrice: 23,
+  },
+];
+
 function HomePage() {
+  const [rentals] = useState(initialState);
+
+  const renderRentals = () => {
+    return rentals.map((rental) => (
+      <RentalCard key={rental._id} rental={rental} />
+    ));
+  };
+
   return (
     <div className="card-list">
       <div className="container">
         <h1 className="page-title">Your Home All Around the World</h1>
-        <div className="row">
-          <div className="col-md-3">
-            <div className="card bi-card">
-              <img
-                className="card-img-top"
-                src="http://via.placeholder.com/350x250"
-                alt="Card Placeholder"
-              />
-              <div className="card-body">
-                <h6 className="card-subtitle mb-0 text-muted">
-                  Whole Apartment &#183; San Francisco
-                </h6>
-                <h5 className="card-title big-font">Awesome Place</h5>
-                <p className="card-text">
-                  $99 per Night &#183; Free Cancelation
-                </p>
-              </div>
-            </div>
-          </div>
-          <div className="col-md-3">
-            <div className="card bi-card">
-              <img
-                className="card-img-top"
-                src="http://via.placeholder.com/350x250"
-                alt="Card Placeholder"
-              />
-              <div className="card-body">
-                <h6 className="card-subtitle mb-0 text-muted">
-                  Whole Apartment &#183; San Francisco
-                </h6>
-                <h5 className="card-title big-font">Awesome Place</h5>
-                <p className="card-text">
-                  $99 per Night &#183; Free Cancelation
-                </p>
-              </div>
-            </div>
-          </div>
-          <div className="col-md-3">
-            <div className="card bi-card">
-              <img
-                className="card-img-top"
-                src="http://via.placeholder.com/350x250"
-                alt="Card Placeholder"
-              />
-              <div className="card-body">
-                <h6 className="card-subtitle mb-0 text-muted">
-                  Shared Apartment &#183; Walnut Creek
-                </h6>
-                <h5 className="card-title big-font">Sunny Place</h5>
-                <p className="card-text">
-                  $79 per Night &#183; Free Cancelation
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
+        <div className="row">{renderRentals()}</div>
       </div>
     </div>
   );
