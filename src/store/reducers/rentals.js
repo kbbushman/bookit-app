3fde68db5d6c9baf7327f3ec89fb6ab415418a42
kbbushman <k.bushman@yahoo.com>
2021-07-03 22:59:19 -0700

    Adds create rental case

--- src/store/reducers/rentals.js
+++ src/store/reducers/rentals.js
@@ -1,9 +1,11 @@
-import { FETCH_RENTALS } from '../../actions';
+import { FETCH_RENTALS, CREATE_RENTAL } from '../../actions';
 
 export function rentals(state = [], action) {
   switch (action.type) {
     case FETCH_RENTALS:
       return action.rentals;
+    case CREATE_RENTAL:
+      return [...state, action.rental];
     default:
       return state;
   }
