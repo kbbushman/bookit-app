0a948f3a4642ef7f06607197d5412134e0541e3b
kbbushman <k.bushman@yahoo.com>
2021-07-04 13:46:23 -0700

    Adds capitalize helper function

---
+++ src/utils/helpers.js
@@ -0,0 +1,8 @@
+export function capitalize(value) {
+  if (!value || typeof value !== 'string') return '';
+
+  return value
+    .split(' ')
+    .map((word) => word[0].toUpperCase() + word.slice(1))
+    .join(' ');
+}
