6ca6c9ab9e5dd8bee7710d3c6896f90e3aa5dd02
kbbushman <k.bushman@yahoo.com>
2021-07-03 22:35:05 -0700

    Adds react-redux Provider with store

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import App from './App';
+import { initStore } from './store';
 
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import './index.scss';
 
+const store = initStore();
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
