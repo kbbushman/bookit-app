3dd88f8b40b145453a7ec7d5b50529ac204a5140
kbbushman <k.bushman@yahoo.com>
2021-07-03 22:59:02 -0700

    Adds createRental action and action creator

--- src/actions/rentals.js
+++ src/actions/rentals.js
@@ -1,10 +1,18 @@
 import { rentalData } from '../store/data';
 
 export const FETCH_RENTALS = 'FETCH_RENTALS';
+export const CREATE_RENTAL = 'CREATE_RENTAL';
 
 export function fetchRentals() {
   return {
     type: FETCH_RENTALS,
     rentals: rentalData,
+  };
+}
+
+export function createRental(rental) {
+  return {
+    type: CREATE_RENTAL,
+    rental,
   };
 }
