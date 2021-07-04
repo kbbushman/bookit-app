71cdc8b0268ddd29a32e3e848c7b62e9c7576a4c
kbbushman <k.bushman@yahoo.com>
2021-07-03 19:00:23 -0700

    Adds router with home, login and register routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Header from './components/shared/Header';
 import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
 
 function App() {
   return (
-    <div>
+    <>
       <Header />
-      <HomePage />
-    </div>
+      <main>
+        <Router>
+          <Switch>
+            <Route exact path="/">
+              <HomePage />
+            </Route>
+            <Route path="/login">
+              <LoginPage />
+            </Route>
+            <Route path="/register">
+              <RegisterPage />
+            </Route>
+          </Switch>
+        </Router>
+      </main>
+    </>
   );
 }
 
