8d004ec447c0eeca17f871cc3d017866b0e42ac8
kbbushman <k.bushman@yahoo.com>
2021-07-05 01:04:42 -0700

    Refactor adds succes/failure cases and updated initialState

--- src/store/reducers/rental.js
+++ src/store/reducers/rental.js
@@ -1,9 +1,15 @@
-import { FETCH_ONE_RENTAL } from 'actions';
+import * as actionTypes from 'actions/actionTypes';
 
-export function rental(state = {}, action) {
+const initialState = { data: {}, isLoading: false, errors: null };
+
+export function rental(state = initialState, action) {
   switch (action.type) {
-    case FETCH_ONE_RENTAL:
-      return action.rental;
+    case actionTypes.FETCH_ONE_RENTAL:
+      return { data: {}, isLoading: true, errors: null };
+    case actionTypes.FETCH_ONE_RENTAL_SUCCESS:
+      return { data: action.rental, isLoading: false, errors: null };
+    case actionTypes.FETCH_ONE_RENTAL_FAILURE:
+      return { data: {}, isLoading: false, errors: action.errors };
     default:
       return state;
   }
--- src/store/reducers/rentals.js
+++ src/store/reducers/rentals.js
@@ -1,11 +1,21 @@
 import * as actionTypes from 'actions/actionTypes';
 
-export function rentals(state = [], action) {
+const initialState = { data: [], isLoading: false, errors: null };
+
+export function rentals(state = initialState, action) {
   switch (action.type) {
     case actionTypes.FETCH_RENTALS:
-      return action.rentals;
+      return { data: [], isLoading: true, errors: null };
+    case actionTypes.FETCH_RENTALS_SUCCESS:
+      return { data: action.rentals, isLoading: false, errors: null };
+    case actionTypes.FETCH_RENTALS_FAILURE:
+      return { data: [], isLoading: false, errors: action.errors };
     case actionTypes.CREATE_RENTAL:
-      return [...state, action.rental];
+      return {
+        data: [...state.rentals, action.rental],
+        isLoading: false,
+        errors: null,
+      };
     default:
       return state;
   }
