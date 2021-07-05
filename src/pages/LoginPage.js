0ae5c0b196a29918e49965b22500a516d1595cfa
kbbushman <k.bushman@yahoo.com>
2021-07-05 16:09:25 -0700

    Adds form views

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,7 +1,50 @@
+import loginImage from 'images/login-image.jpg';
+
 function LoginPage() {
   return (
-    <div>
-      <h1>Login Page</h1>
+    <div className="bi-form">
+      <div className="row">
+        <div className="col-md-5">
+          <h1 className="page-title">Login</h1>
+          {/* <!-- <div className="alert alert-success">
+            Some message
+          </div> --> */}
+          <form>
+            <div className="form-group mb-3">
+              <label htmlFor="email">Email</label>
+              <input type="email" className="form-control" id="email" />
+              {/* <div className="alert alert-danger">
+                <div>
+                  Email is required.
+                </div>
+                <div>
+                  Must be valid email format!
+                </div>
+              </div> */}
+            </div>
+            <div className="form-group mb-3">
+              <label htmlFor="password">Password</label>
+              <input type="password" className="form-control" id="password" />
+            </div>
+            <button type="submit" className="btn btn-bi-main">
+              Submit
+            </button>
+          </form>
+          {/* <div className="alert alert-danger">
+            <p>
+              Some Error
+            </p>
+          </div> --> */}
+        </div>
+        <div className="col-md-6 ml-auto">
+          <div className="image-container">
+            <h2 className="catchphrase">
+              Hundreds of awesome places in reach of few clicks.
+            </h2>
+            <img src={loginImage} alt="Login" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -1,7 +1,59 @@
+import registerImage from 'images/register-image.jpg';
+
 function RegisterPage() {
   return (
-    <div>
-      <h1>Register Page</h1>
+    <div className="bi-form">
+      <div className="row">
+        <div className="col-md-5">
+          <h1 className="page-title">Register</h1>
+          <form>
+            <div className="form-group mb-3">
+              <label htmlFor="username">Username</label>
+              <input type="text" className="form-control" id="username" />
+              {/* <div className="alert alert-danger">
+                <div *ngIf="username.errors.required">
+                  Username is required.
+                </div>
+              </div> */}
+            </div>
+
+            <div className="form-group mb-3">
+              <label htmlFor="email">Email</label>
+              <input type="email" className="form-control" id="email" />
+            </div>
+
+            <div className="form-group mb-3">
+              <label htmlFor="password">Password</label>
+              <input type="password" className="form-control" id="password" />
+            </div>
+
+            <div className="form-group mb-3">
+              <label htmlFor="passwordConfirmation">Confirm Password</label>
+              <input
+                type="password"
+                className="form-control"
+                id="passwordConfirmation"
+              />
+            </div>
+            <button type="submit" className="btn btn-bi-main">
+              Submit
+            </button>
+          </form>
+          {/* <div className="alert alert-danger">
+            <p>
+              Some Error
+            </p>
+          </div> */}
+        </div>
+        <div className="col-md-6 ml-auto">
+          <div className="image-container">
+            <h2 className="catchphrase">
+              As our member you have access to most awesome places in the world.
+            </h2>
+            <img src={registerImage} alt="Register" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
