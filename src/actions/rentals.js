f86e06ed8964dcd41e8f67c1f22f60bcba4b15c5
kbbushman <k.bushman@yahoo.com>
2021-07-05 01:02:36 -0700

    Refactor adds success/failure dispatch

--- src/actions/rentals.js
+++ src/actions/rentals.js
@@ -5,35 +5,63 @@ const BASE_URL = '/api/v1/rentals/';
 
 export function fetchRentals() {
   return async function (dispatch) {
+    dispatch({
+      type: actionTypes.FETCH_RENTALS,
+    });
+
     try {
       const { data } = await axios.get(BASE_URL);
       dispatch({
-        type: actionTypes.FETCH_RENTALS,
+        type: actionTypes.FETCH_RENTALS_SUCCESS,
         rentals: data,
       });
     } catch (err) {
-      // TODO: Handle error
+      dispatch({
+        type: actionTypes.FETCH_RENTALS_FAILURE,
+        errors: err.response.data.errors || err.message,
+      });
     }
   };
 }
 
 export function fetchOneRental(id) {
   return async function (dispatch) {
+    dispatch({
+      type: actionTypes.FETCH_ONE_RENTAL,
+    });
+
     try {
       const { data } = await axios.get(BASE_URL + id);
       dispatch({
-        type: actionTypes.FETCH_ONE_RENTAL,
+        type: actionTypes.FETCH_ONE_RENTAL_SUCCESS,
         rental: data,
       });
     } catch (err) {
-      // TODO: Handle error
+      dispatch({
+        type: actionTypes.FETCH_ONE_RENTAL_FAILURE,
+        errors: err.response.data.errors || err.message,
+      });
     }
   };
 }
 
 export function createRental(rental) {
-  return {
-    type: actionTypes.CREATE_RENTAL,
-    rental,
+  return async function (dispatch) {
+    dispatch({
+      type: actionTypes.CREATE_RENTAL,
+    });
+
+    try {
+      const { data } = await axios.post(BASE_URL, rental);
+      dispatch({
+        type: actionTypes.CREATE_RENTAL_SUCCESS,
+        rental: data,
+      });
+    } catch (err) {
+      dispatch({
+        type: actionTypes.CREATE_RENTAL_FAILURE,
+        errors: err.response.data.errors || err.message,
+      });
+    }
   };
 }
