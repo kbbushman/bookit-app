3288abb184a47983daef7cb0d8e844400d537b10
kbbushman <k.bushman@yahoo.com>
2021-07-06 15:35:03 -0700

    Adds errorHandler middleware

--- server/index.js
+++ server/index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const rentalsRoutes = require('./routes/rentals');
 const usersRoutes = require('./routes/users');
+const { errorHandler } = require('./middlewares');
 
 const app = express();
 const PORT = process.env.PORT || 4000;
 
 // MIDDLEWARE
 app.use(express.json());
+app.use(errorHandler);
 
 // API ROUTES
 app.use('/api/v1/rentals', rentalsRoutes);
