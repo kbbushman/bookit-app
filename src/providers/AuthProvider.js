aba03c06f811ff9e016dc0549bb5e3db42dcfa5e
kbbushman <k.bushman@yahoo.com>
2021-07-06 23:28:17 -0700

    Adds token to local storage and user authenticated dispatch

--- src/providers/AuthProvider.js
+++ src/providers/AuthProvider.js
@@ -1,12 +1,25 @@
 import { createContext } from 'react';
+import { useDispatch } from 'react-redux';
+import jwt from 'jsonwebtoken';
 import { loginUser } from 'actions';
+import { USER_AUTHENTICATED } from 'actions/actionTypes';
 
 const AuthContext = createContext(null);
 
 export function AuthProvider(props) {
+  const dispatch = useDispatch();
+
+  function decodeToken(token) {
+    return jwt.decode(token);
+  }
+
   const logIn = async (formData) => {
-    const { data } = await loginUser(formData);
-    return data.token;
+    const { token } = await loginUser(formData);
+    localStorage.setItem('bi_token', token);
+    const decodedToken = decodeToken(token);
+    console.log(decodedToken);
+    dispatch({ type: USER_AUTHENTICATED, username: decodedToken.username });
+    return token;
   };
 
   const authApi = { logIn };
