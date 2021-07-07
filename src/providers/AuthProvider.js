4119c62c18e0edc683c0879b217dac7e43b0c815
kbbushman <k.bushman@yahoo.com>
2021-07-07 01:35:59 -0700

    Adds logOut function with user logged out dispatch

--- src/providers/AuthProvider.js
+++ src/providers/AuthProvider.js
@@ -2,7 +2,7 @@ import { createContext, useContext } from 'react';
 import { useDispatch } from 'react-redux';
 import jwt from 'jsonwebtoken';
 import { loginUser } from 'actions';
-import { USER_AUTHENTICATED } from 'actions/actionTypes';
+import { USER_AUTHENTICATED, USER_LOGGED_OUT } from 'actions/actionTypes';
 
 const AuthContext = createContext(null);
 
@@ -23,15 +23,20 @@ export function AuthProvider(props) {
     }
   }
 
-  const logIn = async (formData) => {
+  async function logIn(formData) {
     const { token } = await loginUser(formData);
     localStorage.setItem('bi_token', token);
     const { username } = decodeToken(token);
     dispatch({ type: USER_AUTHENTICATED, username });
     return token;
-  };
+  }
 
-  const authApi = { logIn, checkAuthState };
+  function logOut() {
+    localStorage.removeItem('bi_token');
+    dispatch({ type: USER_LOGGED_OUT });
+  }
+
+  const authApi = { logIn, checkAuthState, logOut };
 
   return (
     <AuthContext.Provider value={authApi}>
