1c164d5b7957df752db84df79589f3f42d6b108a
kbbushman <k.bushman@yahoo.com>
2021-07-07 01:37:20 -0700

    Adds logOut to header props

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
 
   return (
     <Router>
-      <Header />
+      <Header logOut={authService.logOut} />
       <main className="container bi-container">
         <Routes />
       </main>
