31c0e2aa4b5398823bd7cafcea3ce06493eba5b5
kbbushman <k.bushman@yahoo.com>
2021-07-07 20:28:20 -0700

    Adds location to TomMap component

--- src/components/rental/RentalMedia.js
+++ src/components/rental/RentalMedia.js
@@ -1,13 +1,18 @@
 import TomMap from '../map/TomMap';
 
 function RentalMedia({ rental }) {
+  function getLocation() {
+    const { street, city } = rental;
+    return street && city && city + ', ' + street;
+  }
+
   return (
     <div className="row">
       <div className="col-md-6">
         <img src={rental.image} alt={rental.title} />
       </div>
       <div className="col-md-6">
-        <TomMap />
+        <TomMap location={getLocation()} />
       </div>
     </div>
   );
