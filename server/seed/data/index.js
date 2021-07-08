0a920f7d4b034624bb072ce0bcd436eab3fa2583
kbbushman <k.bushman@yahoo.com>
2021-07-08 13:19:09 -0700

    Refactor moves rental data to index, adds user data

---
+++ server/seed/data/index.js
@@ -0,0 +1,69 @@
+const mongoose = require('mongoose');
+
+const userData = [
+  {
+    _id: mongoose.Types.ObjectId(),
+    username: 'UserOne',
+    email: 'test1@test.com',
+    password: '123456',
+  },
+  {
+    _id: mongoose.Types.ObjectId(),
+    username: 'UserTwo',
+    email: 'test2@test.com',
+    password: '123456',
+  },
+  {
+    _id: mongoose.Types.ObjectId(),
+    username: 'UserThree',
+    email: 'test3@test.com',
+    password: '123456',
+  },
+];
+
+const rentalData = [
+  {
+    title: 'Nice view on ocean',
+    city: 'San Francisco',
+    street: '2nd St',
+    category: 'condo',
+    image:
+      'https://booksync-jerga-prod.s3.amazonaws.com/uploads/rental/image/5/image.jpeg',
+    numOfRooms: 4,
+    shared: true,
+    description: 'Very nice condo in center of the city.',
+    dailyPrice: 99,
+    owner: userData[0]._id,
+  },
+  {
+    title: 'Modern apartment in center',
+    city: 'New York',
+    street: '5th Avenue',
+    category: 'apartment',
+    image:
+      'https://booksync-jerga-prod.s3.amazonaws.com/uploads/rental/image/5/image.jpeg',
+    numOfRooms: 1,
+    shared: false,
+    description: 'Very nice apartment in center of the city.',
+    dailyPrice: 130,
+    owner: userData[0]._id,
+  },
+  {
+    title: 'Luxurious house in London',
+    city: 'London',
+    street: 'Bull Inn Court,',
+    category: 'house',
+    image:
+      'https://booksync-jerga-prod.s3.amazonaws.com/uploads/rental/image/5/image.jpeg',
+    numOfRooms: 5,
+    shared: false,
+    description: 'Spacious and luxurious house in a very popular neighborhood.',
+    dailyPrice: 150,
+    owner: userData[1]._id,
+  },
+];
+
+module.exports = {
+  userData,
+  rentalData,
+};
