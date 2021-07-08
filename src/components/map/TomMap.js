11889dfeec53b3bec2324e1c08a911c1ae05a283
kbbushman <k.bushman@yahoo.com>
2021-07-07 20:29:37 -0700

    Adds city geocode location search

--- src/components/map/TomMap.js
+++ src/components/map/TomMap.js
@@ -1,12 +1,28 @@
-import { useEffect } from 'react';
+import { useEffect, useCallback, useRef } from 'react';
 import { useMap } from 'providers/MapProvider';
 import './TomMap.scss';
 
-function TomMap() {
-  const { initMap } = useMap();
+function TomMap({ location }) {
+  let map = useRef(null);
+  const { initMap, requestGeoLocation, setCenter } = useMap();
+
+  const getGeoLocation = useCallback(
+    async (location) => {
+      if (location) {
+        const position = await requestGeoLocation(location);
+        console.log(position);
+        setCenter(map.current, position);
+      }
+    },
+    [requestGeoLocation, map, setCenter]
+  );
 
   useEffect(() => {
-    initMap();
+    getGeoLocation(location, map);
+  }, [location, getGeoLocation]);
+
+  useEffect(() => {
+    map.current = initMap();
   }, [initMap]);
 
   return <div id="bi-map"></div>;
