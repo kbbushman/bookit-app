2db81ad6dd77648a5b961f9d1d6aeda66b7fe704
kbbushman <k.bushman@yahoo.com>
2021-07-07 20:26:46 -0700

    Adds geocode location search for city location and map centering api

--- src/providers/MapProvider.js
+++ src/providers/MapProvider.js
@@ -1,4 +1,5 @@
 import { createContext, useContext } from 'react';
+import axios from 'axios';
 import tt from '@tomtom-international/web-sdk-maps';
 
 const MapContext = createContext(null);
@@ -8,11 +9,29 @@ export function MapProvider({ children, apiKey }) {
     const map = tt.map({
       key: apiKey,
       container: 'bi-map',
+      zoom: 15,
     });
     map.addControl(new tt.NavigationControl());
+    return map;
   }
 
-  const mapApi = { initMap };
+  async function requestGeoLocation(location) {
+    const response = await axios.get(
+      `https://api.tomtom.com/search/2/geocode/${location}.JSON?key=${apiKey}`
+    );
+    if (response.data.results.length > 0) {
+      const { position } = response.data.results[0];
+      return position;
+    }
+
+    return Promise.reject('Location not found');
+  }
+
+  function setCenter(map, position) {
+    map.setCenter(new tt.LngLat(position.lon, position.lat));
+  }
+
+  const mapApi = { initMap, requestGeoLocation, setCenter };
 
   return <MapContext.Provider value={mapApi}>{children}</MapContext.Provider>;
 }
