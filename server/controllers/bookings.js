5b8b1232ccb5bf905c94529b8298cb8dd5be8e15
kbbushman <k.bushman@yahoo.com>
2021-07-09 00:23:16 -0700

    Adds booking dates and availability validation

--- server/controllers/bookings.js
+++ server/controllers/bookings.js
@@ -4,14 +4,21 @@ exports.createBooking = async (req, res) => {
   const bookingData = req.body;
   const newBooking = new Booking({ ...bookingData, user: res.locals.user._id });
 
-  try {
-    const rentalBookings = Booking.find({ rental: newBooking.rental });
+  if (!validDates(newBooking)) {
+    return res.sendApiError({
+      title: 'Rental Error',
+      message: 'Requested booking date range is not valid',
+    });
+  }
 
+  try {
+    const rentalBookings = await Booking.find({ rental: newBooking.rental });
     const bookingIsValid = validateBooking(newBooking, rentalBookings);
+
     if (!bookingIsValid) {
       return res.sendApiError({
         title: 'Rental Error',
-        message: 'Requested rental booking is not valid',
+        message: 'Requested booking date range is not available',
       });
     }
 
@@ -23,7 +30,36 @@ exports.createBooking = async (req, res) => {
   }
 };
 
+function validDates(newBooking) {
+  let isValid = true;
+
+  if (!newBooking.startDate || !newBooking.endDate) {
+    isValid = false;
+  }
+
+  if (newBooking.startDate.valueOf() > newBooking.endDate.valueOf()) {
+    isValid = false;
+  }
+
+  return isValid;
+}
+
 function validateBooking(newBooking, rentalBookings) {
-  // TODO: Implement booking validation
-  return true;
+  let isValid = true;
+  if (rentalBookings && rentalBookings.length) {
+    isValid = rentalBookings.every((booking) => {
+      const newBookingStart = newBooking.startDate.valueOf();
+      const newBookingEnd = newBooking.endDate.valueOf();
+
+      const bookingStart = booking.startDate.valueOf();
+      const bookingEnd = booking.endDate.valueOf();
+
+      return (
+        (bookingStart < newBookingStart && bookingEnd < newBookingStart) ||
+        (newBookingEnd < bookingEnd && newBookingEnd < bookingEnd)
+      );
+    });
+  }
+
+  return isValid;
 }
