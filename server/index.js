f4fb2a955d875c5ff521c643bb2ae4aff9f2d5dd
kbbushman <k.bushman@yahoo.com>
2021-07-08 20:31:49 -0700

    Adds bookings routes

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const rentalsRoutes = require('./routes/rentals');
 const usersRoutes = require('./routes/users');
+const bookingsRoutes = require('./routes/bookings');
 const { provideErrorHandlers } = require('./middlewares');
 
 const app = express();
@@ -13,6 +14,7 @@ app.use(provideErrorHandlers);
 // API ROUTES
 app.use('/api/v1/rentals', rentalsRoutes);
 app.use('/api/v1/users', usersRoutes);
+app.use('/api/v1/bookings', bookingsRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
