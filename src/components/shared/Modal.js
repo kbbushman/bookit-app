5fa25866a34b723ace632474a8c3d0932c1aa954
kbbushman <k.bushman@yahoo.com>
2021-07-09 21:22:40 -0700

    Adds submit handler to confirmation button

---
+++ src/components/shared/Modal.js
@@ -0,0 +1,30 @@
+import { Modal } from 'react-responsive-modal';
+import 'react-responsive-modal/styles.css';
+
+function BiModal({ open, onCloseModal, onSubmit, title, subtitle, children }) {
+  return (
+    <Modal
+      open={open}
+      onClose={onCloseModal}
+      classNames={{ modal: 'bi-modal' }}
+    >
+      <h4 className="modal-title title">{title}</h4>
+      <p className="modal-subtitle">{subtitle}</p>
+      <div className="modal-body">{children}</div>
+      <div className="modal-footer">
+        <button type="button" className="btn btn-success" onClick={onSubmit}>
+          Confirm
+        </button>
+        <button
+          type="button"
+          className="btn btn-secondary"
+          onClick={onCloseModal}
+        >
+          Cancel
+        </button>
+      </div>
+    </Modal>
+  );
+}
+
+export default BiModal;
