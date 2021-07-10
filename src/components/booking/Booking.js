359bdefae456d81623918233d0eb82a5751ad270
kbbushman <k.bushman@yahoo.com>
2021-07-09 18:13:39 -0700

    Adss date range picker, adds state for guests input and temp submit handler

--- src/components/booking/Booking.js
+++ src/components/booking/Booking.js
@@ -1,31 +1,112 @@
-function Booking() {
+import { useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { DateRange } from 'react-date-range';
+
+function Booking({ rental }) {
+  const { id } = useParams();
+  const [showDatePicker, setShowDatePicker] = useState(false);
+  const [guests, setGuests] = useState('');
+  const [datesSelected, setDateselected] = useState(null);
+  const [dateRange, setDateRange] = useState([
+    {
+      startDate: new Date(),
+      endDate: new Date(),
+      key: 'selection',
+    },
+  ]);
+
+  function getFormattedDates() {
+    return {
+      startDate: new Date(
+        dateRange[0].startDate.setHours(15, 0, 0, 0)
+      ).toISOString(),
+      endDate: new Date(
+        dateRange[0].endDate.setHours(12, 0, 0, 0)
+      ).toISOString(),
+    };
+  }
+
+  function displayDateRange() {
+    return datesSelected
+      ? `${dateRange[0].startDate.toLocaleDateString()} - ${dateRange[0].endDate.toLocaleDateString()}`
+      : '-- Please select dates --';
+  }
+
+  function handleSetDateRange(dateRangeArray) {
+    setDateRange(dateRangeArray);
+    setDateselected(true);
+  }
+
+  function handleBooking() {
+    console.log({
+      rental: id,
+      guests,
+      ...getFormattedDates(),
+    });
+  }
+
   return (
     <div className="booking">
       <h3 className="booking-price">
-        $12 <span className="booking-per-night">per night</span>
+        ${rental.dailyPrice}{' '}
+        <span className="booking-per-night">per night</span>
       </h3>
       <hr></hr>
       <div className="form-group mb-3">
-        <label htmlFor="dates">Dates</label>
+        <label htmlFor="dates" className="d-block">
+          Dates
+        </label>
         <input
-          placeholder="2020/12/12"
           type="text"
-          className="form-control"
-        ></input>
+          className="form-control text-muted mb-1 pointer"
+          value={displayDateRange()}
+          onClick={() => setShowDatePicker(!showDatePicker)}
+          readOnly
+        />
+        {showDatePicker && (
+          <>
+            <DateRange
+              editableDateInputs={true}
+              onChange={(item) => handleSetDateRange([item.selection])}
+              moveRangeOnFirstSelection={false}
+              showDateDisplay={false}
+              months={1}
+              ranges={dateRange}
+              minDate={new Date()}
+              direction="horizontal"
+            />
+            <div className="d-grid justify-content-md-end">
+              <button
+                className="btn btn-primary"
+                onClick={() => setShowDatePicker(false)}
+              >
+                Close
+              </button>
+            </div>
+          </>
+        )}
       </div>
       <div className="form-group mb-3">
         <label htmlFor="guests">Guests</label>
         <input
-          value="1"
-          min={1}
-          type="number"
           className="form-control"
+          type="number"
           id="guests"
           aria-describedby="guests"
-        ></input>
+          min={1}
+          placeholder="-- Please enter number of guests --"
+          value={guests}
+          onChange={(e) => setGuests(e.target.value)}
+        />
       </div>
       <div className="d-grid">
-        <button className="btn btn-bi-form btn-block">Reserve Now</button>
+        <button
+          className="btn btn-lg btn-bi-form"
+          onClick={handleBooking}
+          disabled={!datesSelected || !guests}
+        >
+          Reserve Now
+        </button>
       </div>
       <hr></hr>
       <p className="booking-note-title">
