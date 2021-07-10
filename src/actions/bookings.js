27a145917e6258b813347a0c554e37cdccffa166
kbbushman <k.bushman@yahoo.com>
2021-07-10 00:59:12 -0700

    Adds getBookings for rental action

--- src/actions/bookings.js
+++ src/actions/bookings.js
@@ -2,7 +2,7 @@ import axiosService from 'services/AxiosService';
 import { extractApiErrors } from 'utils/helpers';
 import {} from './actionTypes';
 
-const BASE_URL = '/bookings/';
+const BASE_URL = '/bookings';
 const { biAxios } = axiosService;
 
 export function createBooking(booking) {
@@ -10,4 +10,8 @@ export function createBooking(booking) {
     .post(BASE_URL, booking)
     .then((res) => res.data)
     .catch((err) => Promise.reject(extractApiErrors(err.response) || {}));
+}
+
+export function getBookings(rentalId) {
+  return biAxios.get(`${BASE_URL}?rental=${rentalId}`).then((res) => res.data);
 }
