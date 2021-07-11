2d76d5a9ac2fbf6a88d2bbf4876a523057f87524
kbbushman <k.bushman@yahoo.com>
2021-07-11 15:38:33 -0700

    Adds get user bookings route

--- server/routes/bookings.js
+++ server/routes/bookings.js
@@ -1,9 +1,14 @@
 const router = require('express').Router();
-const { createBooking, getBookings } = require('../controllers/bookings');
+const {
+  createBooking,
+  getBookings,
+  getUserBookings,
+} = require('../controllers/bookings');
 const { authRequired } = require('../middlewares');
 const { blockRentalOwner } = require('../middlewares/rentals');
 
 router.get('', getBookings);
 router.post('/', authRequired, blockRentalOwner, createBooking);
+router.get('/me', authRequired, getUserBookings);
 
 module.exports = router;
