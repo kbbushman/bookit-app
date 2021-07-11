5785f274345b15cdb0e7dcb669fecd4cc14de29e
kbbushman <k.bushman@yahoo.com>
2021-07-10 23:32:50 -0700

    Adds rental search results page

---
+++ src/pages/RentalSearchPage.js
@@ -0,0 +1,31 @@
+import { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import RentalCard from 'components/rental/RentalCard';
+import { fetchRentals } from 'actions';
+import { capitalize } from '../utils/helpers';
+
+function RentalSearchPage() {
+  const { location } = useParams();
+  const dispatch = useDispatch();
+  const rentals = useSelector((state) => state.rentals.data);
+
+  useEffect(() => {
+    dispatch(fetchRentals(location));
+  }, [dispatch, location]);
+
+  const renderRentals = () => {
+    return rentals.map((rental) => (
+      <RentalCard key={rental._id} rental={rental} />
+    ));
+  };
+
+  return (
+    <div className="card-list">
+      <h1 className="page-title">Results for {capitalize(location)}</h1>
+      <div className="row">{renderRentals()}</div>
+    </div>
+  );
+}
+
+export default RentalSearchPage;
