ad3f562b2b387b1384623a5445a239ac217b0c83
kbbushman <k.bushman@yahoo.com>
2021-07-11 17:44:17 -0700

    Adds delete user route

--- server/routes/bookings.js
+++ server/routes/bookings.js
@@ -4,13 +4,15 @@ const {
   getBookings,
   getUserBookings,
   getOwnerBookings,
+  deleteBooking,
 } = require('../controllers/bookings');
 const { authRequired } = require('../middlewares');
 const { blockRentalOwner } = require('../middlewares/rentals');
 
-router.get('', getBookings);
+router.get('/', getBookings);
 router.post('/', authRequired, blockRentalOwner, createBooking);
 router.get('/me', authRequired, getUserBookings);
 router.get('/owner', authRequired, getOwnerBookings);
+router.delete('/:id', authRequired, deleteBooking);
 
 module.exports = router;
