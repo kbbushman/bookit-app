4949f6a42b8d22083df5c59f455fdad5d78c4c2b
kbbushman <k.bushman@yahoo.com>
2021-07-11 18:08:43 -0700

    Adds delete owner rental controller

--- server/controllers/rentals.js
+++ server/controllers/rentals.js
@@ -1,4 +1,4 @@
-const { Rental } = require('../models');
+const { Rental, Booking } = require('../models');
 
 exports.getRentals = async (req, res) => {
   const { city } = req.query;
@@ -64,10 +64,40 @@ exports.updateRental = async (req, res) => {
 };
 
 exports.deleteRental = async (req, res) => {
-  // TODO: Finish delete
-  return res.sendApiError({
-    title: 'Delete Rental Error',
-    status: 500,
-    message: 'This rental delete endpoint has not been completed.',
-  });
+  const { id } = req.params;
+  const { user } = res.locals;
+
+  try {
+    const rental = await Rental.findById(id).populate('owner');
+
+    if (!rental) {
+      return res.sendApiError({
+        title: 'Rental Delete Error',
+        message: 'This requested rental does not exist',
+      });
+    }
+
+    if (user.id !== rental.owner.id) {
+      return res.sendApiError({
+        title: 'Rental Delete Error',
+        message: 'You do not have permission to delete this rental',
+      });
+    }
+
+    const bookings = await Booking.find({ rental });
+
+    if (bookings.length) {
+      // TODOD: Check for active booking dates
+      return res.sendApiError({
+        title: 'Rental Delete Error',
+        message: 'You cannot delete a rental with active bookings',
+      });
+    }
+
+    await rental.remove();
+
+    res.json({ id });
+  } catch (err) {
+    res.sendMongoError(err);
+  }
 };
