96d90bd1fa9b0afbb0154286e48a829ec2773ed8
kbbushman <k.bushman@yahoo.com>
2021-07-12 00:09:34 -0700

    Adds manage combined reducer

--- src/store/index.js
+++ src/store/index.js
@@ -3,12 +3,14 @@ import thunk from 'redux-thunk';
 import { rentals } from './reducers/rentals';
 import { rental } from './reducers/rental';
 import { auth } from './reducers/auth';
+import { manage } from './reducers/manage';
 
 export function initStore() {
   const reducer = combineReducers({
     rentals,
     rental,
     auth,
+    manage,
   });
 
   const composeEnhancers =
