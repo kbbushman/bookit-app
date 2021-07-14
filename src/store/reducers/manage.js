908056c208accd31d0f7d7593d9d87221f0a331e
kbbushman <k.bushman@yahoo.com>
2021-07-13 17:30:21 -0700

    Adds cases for fetch user rentals success/failure

--- src/store/reducers/manage.js
+++ src/store/reducers/manage.js
@@ -1,9 +1,20 @@
 import { combineReducers } from 'redux';
+import {
+  FETCH_USER_RENTALS,
+  FETCH_USER_RENTALS_SUCCESS,
+  FETCH_USER_RENTALS_FAILURE,
+} from 'actions/actionTypes';
 
 const initialState = { items: [], isLoading: false, errors: null };
 
 function rentals(state = initialState, action) {
   switch (action.type) {
+    case FETCH_USER_RENTALS:
+      return { items: [], isLoading: true, errors: false };
+    case FETCH_USER_RENTALS_SUCCESS:
+      return { items: action.rentals, isLoading: false, errors: null };
+    case FETCH_USER_RENTALS_FAILURE:
+      return { items: [], isLoading: false, errors: action.errors };
     default:
       return state;
   }
