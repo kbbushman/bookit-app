799cc90653ece3f462298598f2af90e007bef409
kbbushman <k.bushman@yahoo.com>
2021-07-16 22:21:28 -0700

    Adds BookingList component, fetchUserBookings dispatch, and bookings state

--- src/pages/ManageBookings.js
+++ src/pages/ManageBookings.js
@@ -1,7 +1,22 @@
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import BookingList from 'components/booking/BookingList';
+import { fetchUserBookings } from 'actions';
+
 function ManageBookings() {
+  const dispatch = useDispatch();
+  const { items: bookings } = useSelector((state) => state.manage.bookings);
+
+  console.log(bookings);
+
+  useEffect(() => {
+    dispatch(fetchUserBookings());
+  }, [dispatch]);
+
   return (
     <div>
       <h1>Manage Bookings</h1>
+      <BookingList />
     </div>
   );
 }
