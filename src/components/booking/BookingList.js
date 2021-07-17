e35298f2b9a44e4ff26c981a02721c197298fbae
kbbushman <k.bushman@yahoo.com>
2021-07-17 13:42:45 -0700

    Adds destructured booking to template

--- src/components/booking/BookingList.js
+++ src/components/booking/BookingList.js
@@ -1,29 +1,44 @@
-function BookingList() {
+import { Link } from 'react-router-dom';
+import { capitalize } from 'utils/helpers';
+
+function BookingList({ bookings, type, title }) {
   return (
     <section className="booking-listing">
-      <h1 className="page-title">Page Title</h1>
+      <h1 className="page-title">{title}</h1>
       <div className="row">
-        {/* Iterate Bookings */}
-        <div className="col-md-4">
-          <div className="card text-center">
-            {/* Only if 'received' booking */}
-            <div className="card-header">From: Some User</div>
-            {/* Only if 'received' booking END */}
-            <div className="card-block">
-              <h4 className="card-title">Rental Title - Rental City </h4>
-              <p className="card-text booking-days">
-                2020/02/02 - 2020/02/07 | 5 nights
-              </p>
-              <p className="card-text">
-                <span>Price: </span>{' '}
-                <span className="booking-price-value">$250</span>
-              </p>
-              <a className="btn bwm-btn-main">Go to Rental</a>
-              <button className="ml-1 btn btn-danger">Delete</button>
+        {bookings.map((booking) => (
+          <div key={booking._id} className="col-md-4">
+            <div className="card text-center">
+              {type === 'received' && (
+                <div className="card-header">From: Some User</div>
+              )}
+              <div className="card-block">
+                <h4 className="card-title">
+                  {booking.rental.title} - {capitalize(booking.rental.city)}{' '}
+                </h4>
+                <p className="card-text booking-days">
+                  {new Date(booking.startDate).toLocaleDateString()} -{' '}
+                  {new Date(booking.endDate).toLocaleDateString()} |{' '}
+                  {booking.nights} nights
+                </p>
+                <p className="card-text">
+                  <span>Price: </span>{' '}
+                  <span className="booking-price-value">${booking.price}</span>
+                </p>
+                <button className="btn btn-danger me-3">Delete</button>
+                <Link
+                  to={`/rentals/${booking.rental._id}`}
+                  className="btn btn-bi-form"
+                >
+                  Go to Rental
+                </Link>
+              </div>
+              <div className="card-footer text-muted">
+                Created on {new Date(booking.createdAt).toLocaleDateString()}
+              </div>
             </div>
-            <div className="card-footer text-muted">Created at 2020/02/01</div>
           </div>
-        </div>
+        ))}
       </div>
     </section>
   );
