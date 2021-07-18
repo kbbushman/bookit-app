3a4c55df94534aff2df2fb3298731a9b09fc3eba
kbbushman <k.bushman@yahoo.com>
2021-07-17 22:53:23 -0700

    Adds verify user route

--- server/routes/rentals.js
+++ server/routes/rentals.js
@@ -7,11 +7,13 @@ const {
   updateRental,
   deleteRental,
   getUserRentals,
+  verifyUser,
 } = require('../controllers/rentals');
 
 router.get('/', getRentals);
 router.get('/me', authRequired, getUserRentals);
 router.get('/:id', getRentalById);
+router.get('/:id/verify-user', authRequired, verifyUser);
 router.post('/', authRequired, createRental);
 router.put('/:id', authRequired, updateRental);
 router.delete('/:id', authRequired, deleteRental);
