4fec8667fcfad26b556d907b1d1c8d6aa3216b57
kbbushman <k.bushman@yahoo.com>
2021-07-18 23:30:57 -0700

    Adds update rental cases

--- src/store/reducers/rental.js
+++ src/store/reducers/rental.js
@@ -2,6 +2,8 @@ import {
   FETCH_ONE_RENTAL,
   FETCH_ONE_RENTAL_SUCCESS,
   FETCH_ONE_RENTAL_FAILURE,
+  UPDATE_RENTAL,
+  UPDATE_RENTAL_SUCCESS,
 } from 'actions/actionTypes';
 
 const initialState = { data: {}, isLoading: false, errors: null };
@@ -14,6 +16,10 @@ export function rental(state = initialState, action) {
       return { data: action.rental, isLoading: false, errors: null };
     case FETCH_ONE_RENTAL_FAILURE:
       return { data: {}, isLoading: false, errors: action.errors };
+    case UPDATE_RENTAL:
+      return { data: state.data, isLoading: true, errors: null };
+    case UPDATE_RENTAL_SUCCESS:
+      return { data: action.rental, isLoading: false, errors: null };
     default:
       return state;
   }
