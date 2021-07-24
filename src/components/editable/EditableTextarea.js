8fd6f3615f40e83fb0c5276707bc8493d54d2248
kbbushman <k.bushman@yahoo.com>
2021-07-23 18:53:58 -0700

    Adds editable textarea with state and dispatch

---
+++ src/components/editable/EditableTextarea.js
@@ -0,0 +1,90 @@
+import { useState } from 'react';
+
+function EditableTextarea({
+  entity,
+  field,
+  className,
+  handleUpdateRental,
+  transformView,
+  rows,
+  cols,
+}) {
+  const [value, setValue] = useState(entity[field]);
+  const [originalValue, setOriginalValue] = useState(entity[field]);
+  const [isActiveInput, setIsActiveInput] = useState(false);
+
+  function handleCancel() {
+    setIsActiveInput(false);
+    setValue(originalValue);
+  }
+
+  function onSuccess() {
+    setOriginalValue(value);
+    setIsActiveInput(false);
+  }
+
+  function onError() {
+    setValue(originalValue);
+    setIsActiveInput(false);
+  }
+
+  async function update() {
+    if (value !== originalValue) {
+      const rentalData = { [field]: value };
+      handleUpdateRental(rentalData, onSuccess, onError);
+    }
+  }
+
+  function renderView() {
+    if (isActiveInput) {
+      return (
+        <>
+          <textarea
+            className={`editable-item ${className}`}
+            rows={rows}
+            cols={cols}
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+          />
+          <div className="button-container">
+            <button
+              onClick={handleCancel}
+              className="btn btn-sm btn-danger ms-3 btn-editable"
+              autoFocus={false}
+            >
+              Cancel
+            </button>
+            <button
+              onClick={update}
+              className="btn btn-sm btn-success ms-3 btn-editable"
+              autoFocus
+            >
+              Save
+            </button>
+          </div>
+        </>
+      );
+    }
+
+    return (
+      <>
+        <span className={`editable-item ${className}`}>
+          {transformView ? transformView(value) : value}
+        </span>
+        <div className="button-container">
+          <button
+            onClick={() => setIsActiveInput(true)}
+            className="btn btn-sm btn-warning btn-editable"
+            autoFocus={false}
+          >
+            Edit
+          </button>
+        </div>
+      </>
+    );
+  }
+
+  return <div className="editable-component">{renderView()}</div>;
+}
+
+export default EditableTextarea;
