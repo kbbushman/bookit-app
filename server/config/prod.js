15a5cb4c7480314f0d2489d0d847a42fd9caac42
kbbushman <k.bushman@yahoo.com>
2021-07-27 21:39:27 -0700

    Adds cloudinary vars

--- server/config/prod.js
+++ server/config/prod.js
@@ -2,4 +2,7 @@ module.exports = {
   MONGODB_URI: process.env.MONGODB_URI,
   JWT_SECRET: process.env.JWT_SECRET,
   JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN,
+  CLOUDINARY_NAME: process.env.CLOUDINARY_NAME,
+  CLOUNDINARY_KEY: process.env.CLOUDINARY_KEY,
+  CLOUNDINARY_SECRET: process.env.CLOUDINARY_SECRET,
 };
