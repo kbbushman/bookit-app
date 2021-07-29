7573790227413da70077144a92f29d06eafc9493
kbbushman <k.bushman@yahoo.com>
2021-07-28 23:48:23 -0700

    Adds multer config with memory storage

---
+++ server/services/multer.js
@@ -0,0 +1,17 @@
+const multer = require('multer');
+
+const ALLOWED_FORMAT = ['image/jpg', 'image/png', 'image/jpg'];
+
+const storage = multer.memoryStorage();
+const upload = multer({
+  storage,
+  fileFilter: function (req, file, cb) {
+    if (ALLOWED_FORMAT.includes(file.mimetype)) {
+      cb(null, true);
+    } else {
+      cb(new Error('File format is not supported', false));
+    }
+  },
+});
+
+module.exports = upload;
