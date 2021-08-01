19cac23db05e2796b5338dc8dd5cfa24daa23360
kbbushman <k.bushman@yahoo.com>
2021-07-31 23:59:09 -0700

    Adds multer service import

--- server/routes/imageUpload.js
+++ server/routes/imageUpload.js
@@ -1,5 +1,8 @@
 const router = require('express').Router();
 const { authRequired } = require('../middlewares');
+const upload = require('../services/multer');
+const singleUpload = upload.single('image');
+
 
 router.post('/', authRequired, (res, res) => {
     res.json({message: 'Uploading image...'})
