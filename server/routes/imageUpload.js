49b5636f91c340046cce59dba1f6c9e064614cac
kbbushman <k.bushman@yahoo.com>
2021-08-01 23:59:07 -0700

    Adds single image upload controller with log test

--- server/routes/imageUpload.js
+++ server/routes/imageUpload.js
@@ -3,9 +3,34 @@ const { authRequired } = require('../middlewares');
 const upload = require('../services/multer');
 const singleUpload = upload.single('image');
 
+const singleUploadCtrl = (req, res, next) => {
+  singleUpload(req, res, (err) => {
+    if (err) {
+      return res.sendApiError({
+        title: 'Upload Error',
+        message: err.message,
+      });
+    }
 
-router.post('/', authRequired, (res, res) => {
-    res.json({message: 'Uploading image...'})
+    next();
+  });
+};
+
+router.post('/', authRequired, singleUploadCtrl, (res, res) => {
+  try {
+    if (!req.file) {
+      throw new Error('No image was found in upload');
+    }
+
+    console.log(req.file);
+
+    res.json({ message: 'Uploading image...' });
+  } catch (err) {
+    return res.sendApiError({
+      title: 'Upload Error',
+      message: 'Oops! Something went wrong. Please try again',
+    });
+  }
 });
 
 module.exports = router;
