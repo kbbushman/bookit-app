a5d1e0d1350dc3281f1439807173927f8b32ec42
kbbushman <k.bushman@yahoo.com>
2021-08-04 23:57:22 -0700

    Adds dataUri service for buffer to base64 image conversion

---
+++ server/services/dataUri.js
@@ -0,0 +1,7 @@
+// const path = require('path');
+const Datauri = require('datauri');
+const dUri = new Datauri();
+
+exports.dataUri = function (file) {
+  //   return dUri.format(path.extname(file.originalname).toString(), file.buffer);
+};
