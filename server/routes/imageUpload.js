200358a98837bd1ea5a9cd51cde639d78ee9cbae
kbbushman <k.bushman@yahoo.com>
2021-08-08 18:30:57 -0700

    Adds image upload controller with test logs

--- server/routes/imageUpload.js
+++ server/routes/imageUpload.js
@@ -1,6 +1,9 @@
 const router = require('express').Router();
 const { authRequired } = require('../middlewares');
 const upload = require('../services/multer');
+const { dataUri } = require('../services/dataUri');
+const { cloudUpload } = require('../services/cloudinary');
+
 const singleUpload = upload.single('image');
 
 const singleUploadCtrl = (req, res, next) => {
@@ -16,14 +19,19 @@ const singleUploadCtrl = (req, res, next) => {
   });
 };
 
-router.post('/', authRequired, singleUploadCtrl, (req, res) => {
+router.post('/', authRequired, singleUploadCtrl, async (req, res) => {
   try {
     if (!req.file) {
       throw new Error('No image was found in upload');
     }
 
     console.log(req.file);
-
+    const base64Image = dataUri(req.file);
+    const result = await cloudUpload(
+      base64Image.content,
+      req.file.originalname
+    );
+    console.log(result);
     res.json({ message: 'Uploading image...' });
   } catch (err) {
     return res.sendApiError({
