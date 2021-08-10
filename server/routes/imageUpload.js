3a5c6e8cc94ed7d42e69a8fdb80a5f0850313c10
kbbushman <k.bushman@yahoo.com>
2021-08-09 22:40:53 -0700

    Removes console logs

--- server/routes/imageUpload.js
+++ server/routes/imageUpload.js
@@ -26,14 +26,13 @@ router.post('/', authRequired, singleUploadCtrl, async (req, res) => {
       throw new Error('No image was found in upload');
     }
 
-    console.log(req.file);
     const base64Image = dataUri(req.file);
     const result = await cloudUpload(base64Image.content);
     const savedImage = await CloudinaryImage.create({
       url: result.secure_url,
       cloudinaryId: result.public_id,
     });
-    console.log(savedImage);
+
     res.json({ _id: savedImage._id, url: savedImage.url });
   } catch (err) {
     return res.sendApiError({
