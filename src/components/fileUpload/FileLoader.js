801a92140843e080c32bd3c2e6039421ed3436d9
kbbushman <k.bushman@yahoo.com>
2021-08-11 23:35:49 -0700

    Adds file upload styles

--- src/components/fileUpload/FileLoader.js
+++ src/components/fileUpload/FileLoader.js
@@ -1,18 +1,22 @@
+import './FileLoader.scss';
+
 function FileLoader() {
   function handleChange(event) {
     const file = event.target.files[0];
   }
 
   return (
-    <>
-      <label className="upload-text">Select an image</label>
-      <input
-        className="form-control"
-        type="file"
-        accept=".jpg, .png, .jpeg"
-        onChange={handleChange}
-      />
-    </>
+    <div className="img-upload-container">
+      <label className="img-upload btn btn-bi-form">
+        <span className="upload-text">Select an image</span>
+        <input
+          className="file-input"
+          type="file"
+          accept=".jpg, .png, .jpeg"
+          onChange={handleChange}
+        />
+      </label>
+    </div>
   );
 }
 
