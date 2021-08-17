fa45126c30a839416450f2e15e8533f4514b14c0
kbbushman <k.bushman@yahoo.com>
2021-08-16 22:57:12 -0700

    Adds cancel image upload functionality

--- src/components/fileUpload/FileLoader.js
+++ src/components/fileUpload/FileLoader.js
@@ -1,8 +1,9 @@
-import { useState, useEffect, useMemo } from 'react';
+import { useState, useEffect, useMemo, useRef } from 'react';
 import { uploadImage } from 'actions/imageupload';
 import './FileLoader.scss';
 
 function FileLoader() {
+  const imageInputRef = useRef();
   const [base64Image, setBase64image] = useState('');
   const [imageStatus, setImageStatus] = useState('INIT');
   const [selectedImage, setSelectedImage] = useState(null);
@@ -41,6 +42,13 @@ function FileLoader() {
       });
   }
 
+  function cancelImageUpload() {
+    imageInputRef.current.value = null;
+    setSelectedImage(null);
+    setBase64image('');
+    setImageStatus('INIT');
+  }
+
   return (
     <div className="img-upload-container">
       <label className="img-upload btn btn-bi-form">
@@ -50,6 +58,7 @@ function FileLoader() {
           type="file"
           accept=".jpg, .png, .jpeg"
           onChange={handleChange}
+          ref={imageInputRef}
         />
       </label>
       {base64Image && (
@@ -75,19 +84,21 @@ function FileLoader() {
             )}
           </div>
           {imageStatus === 'LOADED' && (
-            <>
-              <button
-                className="btn btn-success me-2"
-                type="button"
-                onClick={handleImageUpload}
-              >
-                Upload
-              </button>
-              <button className="btn btn-danger" type="button">
-                Cancel
-              </button>
-            </>
+            <button
+              className="btn btn-success me-2"
+              type="button"
+              onClick={handleImageUpload}
+            >
+              Upload
+            </button>
           )}
+          <button
+            className="btn btn-danger"
+            type="button"
+            onClick={cancelImageUpload}
+          >
+            Cancel
+          </button>
         </>
       )}
     </div>
