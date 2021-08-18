475de08de51bdd47be7b164f894a8c5270707797
kbbushman <k.bushman@yahoo.com>
2021-08-17 23:19:51 -0700

    Adds spinner component

---
+++ src/components/shared/Spinner.js
@@ -0,0 +1,16 @@
+import './Spinner.css';
+
+function Spinner() {
+  return (
+    <div className="sk-chase">
+      <div className="sk-chase-dot"></div>
+      <div className="sk-chase-dot"></div>
+      <div className="sk-chase-dot"></div>
+      <div className="sk-chase-dot"></div>
+      <div className="sk-chase-dot"></div>
+      <div className="sk-chase-dot"></div>
+    </div>
+  );
+}
+
+export default Spinner;
